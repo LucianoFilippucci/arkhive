16d33bdc1afa8895c52b8cb26d4f24b1e3cabbf0
Luciano Filippucci <filippucci.luciano@outlook.it>
2025-05-23 20:46:56 +0200

    Initial Project Settings

---
+++ src/main/java/it/arkhive/arkhive/ArkhiveApplication.java
@@ -0,0 +1,13 @@
+package it.arkhive.arkhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkhiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArkhiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/arkhive/arkhive/ArkhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package it.arkhive.arkhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkhiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
